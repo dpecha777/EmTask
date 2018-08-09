fbfa8e001b84aae9cdc31622e4631d047d0ea241
Dominik Pecha <weedy@DominikPecha.local>
2018-08-09 12:46:52 +0200

    first page completed

--- app/router.js
+++ app/router.js
@@ -7,10 +7,12 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('header', { path: '/' });
   this.route('tasks', function() {
     this.route('new');
     this.route('edit', {path: '/edit/:task_id'});
   });
+  
 });
 
 export default Router;
