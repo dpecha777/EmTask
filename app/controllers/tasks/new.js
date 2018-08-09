41c506962971d1f5e020d568c2ab1064cced65ef
Dominik Pecha <weedy@DominikPecha.local>
2018-08-09 14:24:16 +0200

    tasks edit adjustment

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -24,6 +24,8 @@ export default Controller.extend({
                 description: '',
                 date: ''
             });
+
+            this.transitionToRoute('tasks');
         }
     }
 });
